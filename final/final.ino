576d4cf8f97163eb8b75411e72eedf71f6df7deb
parkero2 <72895391+parkero2@users.noreply.github.com>
2023-08-03 10:59:24 +1200

    reformatted

--- final/final.ino
+++ final/final.ino
@@ -4,7 +4,7 @@
 // Other libaries, included in the project folder
 #include "include/LCD_I2C/src/LCD_I2C.h"
 #include "include/TinyGPSPlus/src/TinyGPSPlus.h"
-#include "include/IRremote/src/IRremote.h"
+// #include "include/IRremote/src/IRremote.h"
 
 /*
    This sample sketch demonstrates the normal use of a TinyGPSPlus (TinyGPSPlus) object.
@@ -16,9 +16,9 @@ static const uint32_t GPSBaud = 9600;
 
 // The TinyGPSPlus object
 TinyGPSPlus gps;
-LCD_I2C lcd(0x27, 16, 2); // Default address of most PCF8574 modules, change according 
+LCD_I2C lcd(0x27, 16, 2); // Default address of most PCF8574 modules, change according
 
-//updateGPS(); will reset Serial communication between the Arduino and the GPS module
+// updateGPS(); will reset Serial communication between the Arduino and the GPS module
 
 double lat1, lat2, lon1, lon2;
 double latR1, latR2, lonR1, lonR2;
@@ -33,22 +33,23 @@ bool setStart = true;
 int setLocation; // Pin to set the current location
 
 /*-----------------------------Setup---------------------------------*/
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(115200);
-   lcd.begin(); // If you are using more I2C devices using the Wire library use lcd.begin(false)
-                 // this stop the library(lcd_I2C) from calling Wire.begin()
-    lcd.backlight();
+  lcd.begin(); // If you are using more I2C devices using the Wire library use lcd.begin(false)
+               // this stop the library(lcd_I2C) from calling Wire.begin()
+  lcd.backlight();
   Serial1.begin(GPSBaud);
 
-  //displayInfo();
+  // displayInfo();
   lcd.clear();
   pinMode(setLocation, INPUT_PULLUP); // Connect 1k resistor between pin and +5V
 }
 
 void displayInfo()
 {
- 
+
   if (gps.location.isValid())
   {
     lcd.clear();
@@ -58,32 +59,37 @@ void displayInfo()
     lcd.println(gps.location.lng());
     delay(10000);
   }
- 
+
   Serial.println();
 }
 
-void updateGPS() {
-  //Reset GPS communication
+void updateGPS()
+{
+  // Reset GPS communication
   Serial1.begin(GPSBaud);
-  while (Serial1.available() > 0) {
+  while (Serial1.available() > 0)
+  {
     if (gps.encode(Serial1.read()))
       displayInfo();
   }
 }
 
 /*-----------------------------Main Loop---------------------------------*/
-void loop() {
+void loop()
+{
 
-  //Setting the start/end location
-  if (!digitalRead(setLocation)) {
+  // Setting the start/end location
+  if (!digitalRead(setLocation))
+  {
     lcd.clear();
-    //Set the current location
+    // Set the current location
     setStart ? lat1 = gps.location.lat() : lat2 = gps.location.lat();
     setStart ? lon1 = gps.location.lng() : lon2 = gps.location.lng();
-    String Start = setStart ? "Start loc: "  + String(lat1, 6) + ", " + String(lon1, 6) : "End loc" + String(lat1, 6) + ", " + String(lon1, 6);
+    String Start = setStart ? "Start loc: " + String(lat1, 6) + ", " + String(lon1, 6) : "End loc" + String(lat1, 6) + ", " + String(lon1, 6);
     lcd.print(Start);
     setStart = !setStart;
-    while (!digitalRead(setLocation));
+    while (!digitalRead(setLocation))
+      ;
   }
 
   // This sketch displays information every time a new sentence is correctly encoded.
@@ -92,71 +98,74 @@ void loop() {
   {
     Serial.println(F("No GPS detected: check wiring."));
     delay(1000);
-    while(true);
+    while (true)
+      ;
   }
-  while(!Serial1.available() > 0);
-  while (Serial1.available() > 0) {
+  while (!Serial1.available() > 0)
+    ;
+  while (Serial1.available() > 0)
+  {
     if (gps.encode(Serial1.read()))
       displayInfo();
   }
-  //Get Starting GPS Coordinates
+  // Get Starting GPS Coordinates
   lon1 = gps.location.lng();
   lat1 = gps.location.lat();
   String Start = "GPS Starting Location: " + String(lat1, 6) + ", " + String(lon1, 6);
   lcd.print(Start);
   delay(10000);
 
-  //Get Destination GPS Coordinates
+  // Get Destination GPS Coordinates
   lat2 = -37.677285;
   lon2 = 176.130491;
   String Dest = "GPS Destination Location: " + String(lat2, 6) + ", " + String(lon2, 6);
   Serial.print(Dest);
   delay(10000);
 
-  calcdist(); //Call the distance and bearing calculation function
-
+  calcdist(); // Call the distance and bearing calculation function
 }
 
 /*-----------------------------Distance & Bearing Calculator---------------------------------*/
-void calcdist(){ //This is a haversine based distance calculation formula
-  //This portion converts the current and destination GPS coords from decDegrees to Radians
-  lonR1 = lon1*(PI/180);
-  lonR2 = lon2*(PI/180);
-  latR1 = lat1*(PI/180);
-  latR2 = lat2*(PI/180);
-  
-  //This portion calculates the differences for the Radian latitudes and longitudes and saves them to variables
+void calcdist()
+{ // This is a haversine based distance calculation formula
+  // This portion converts the current and destination GPS coords from decDegrees to Radians
+  lonR1 = lon1 * (PI / 180);
+  lonR2 = lon2 * (PI / 180);
+  latR1 = lat1 * (PI / 180);
+  latR2 = lat2 * (PI / 180);
+
+  // This portion calculates the differences for the Radian latitudes and longitudes and saves them to variables
   dlon = lonR2 - lonR1;
   dlat = latR2 - latR1;
-  
-  //This portion is the Haversine Formula for distance between two points. Returned value is in KM
-  a = (sq(sin(dlat/2))) + cos(latR1) * cos(latR2) * (sq(sin(dlon/2)));
-  e = 2 * atan2(sqrt(a), sqrt(1-a)) ;
+
+  // This portion is the Haversine Formula for distance between two points. Returned value is in KM
+  a = (sq(sin(dlat / 2))) + cos(latR1) * cos(latR2) * (sq(sin(dlon / 2)));
+  e = 2 * atan2(sqrt(a), sqrt(1 - a));
   d = R * e;
 
   lcd.println();
   Serial.print("Distance to destination(KM): ");
-  //lcd.println(a);
-  //lcd.println(e);
+  // lcd.println(a);
+  // lcd.println(e);
   lcd.println(d, 6);
   lcd.println();
-  
-  //This portion is the Haversine Formula for required bearing between current location and destination. Returned value is in Degrees
-  double x = cos(latR2)*sin(lonR2-lonR1); //calculate x
-  
+
+  // This portion is the Haversine Formula for required bearing between current location and destination. Returned value is in Degrees
+  double x = cos(latR2) * sin(lonR2 - lonR1); // calculate x
+
   lcd.print("X = ");
   lcd.println(x, 6);
 
-  double y = cos(latR1)*sin(latR2)-sin(latR1)*cos(latR2)*cos(lonR2-lonR1); //calculate y
+  double y = cos(latR1) * sin(latR2) - sin(latR1) * cos(latR2) * cos(lonR2 - lonR1); // calculate y
 
   lcd.print("Y = ");
   lcd.println(y, 6);
-  float brRad = atan2(x, y); //return atan2 result for bearing. Result at this point is in Radians
+  float brRad = atan2(x, y); // return atan2 result for bearing. Result at this point is in Radians
 
   lcd.print("atan2(x, y) (Radians) = ");
   lcd.println(brRad, 6);
 
-  float reqBear = toDegrees*brRad;
+  float reqBear = toDegrees * brRad;
   lcd.print("Bearing: ");
   lcd.println(reqBear, 4);
 }
